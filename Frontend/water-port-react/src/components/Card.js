e89d967997b36b1cf2e491cccc49cef04dc0761b
ChampManZ <champ_tnt@hotmail.com>
2023-03-22 15:04:28 +0700

    card draft finish

--- Frontend/water-port-react/src/components/Card.js
+++ Frontend/water-port-react/src/components/Card.js
@@ -2,15 +2,35 @@ import React from 'react'
 import '../style/flexbox.css'
 
 function Card() {
-  return (
-    <div className='card-container'>
-        <div className='wqi-container'>
-            <div>Image</div>
-            <div>98 WQI</div>
-            <div>Excellent</div>
+
+    const cardStyle = {
+        width: "400px",
+        boxShadow: "0px 0px 15px -5px",
+        margin: "auto"
+    }
+
+    const wqiStyle = {
+        height: "150px",
+        backgroundColor: "#95F39F"
+    }
+
+    return (
+        <div style={cardStyle}>
+            <div className='wqi-container' style={wqiStyle}>
+                <div>Image</div>
+                <div>
+                    <div>98 WQI</div>
+                    <div>Excellent</div>
+                </div>
+            </div>
+            <div className='parameter-container'>
+                <div>Temp</div>
+                <div>pH</div>
+                <div>Turbidity</div>
+                <div>Conductivity</div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Card
--- Frontend/water-port-react/src/pages/Dashboard.js
+++ Frontend/water-port-react/src/pages/Dashboard.js
@@ -15,7 +15,9 @@ function Dashboard() {
             <div class="flex-container">
                 <div class="flex-left">
                     <h3 style={fontFamilyStyle}>Your Water Quality</h3>
-                    <Card />
+                    <Card /> <br />
+                    <button type='button'>Measure again</button>
+                    <h3>Water Quality Index</h3>
                 </div>
                 <div class="flex-right">
                     This will have content in the right box.
