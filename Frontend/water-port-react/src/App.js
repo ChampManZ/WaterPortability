e4a7cce1a4fd63c8e637beb532409b32b92baad8
ChampManZ <champ_tnt@hotmail.com>
2023-03-22 22:52:40 +0700

    add draft wqi

--- Frontend/water-port-react/src/App.js
+++ Frontend/water-port-react/src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='dashboard' element={<Dashboard />} />
-
+        
         </Routes>
       </BrowserRouter>
       {/* <Dashboard/> 
